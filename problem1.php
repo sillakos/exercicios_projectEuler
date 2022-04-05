c47e0a891f12b57241ed658a1cc464fe4c46ad5e
Sillas Ouriques dos Santos <sillasods@gmail.com>
2022-04-05 14:54:44 -0300

    Refatoração Exercício 1

--- problem1.php
+++ problem1.php
@@ -4,7 +4,24 @@
 A soma desses múltiplos é 23.
 
 Encontre a soma de todos os múltiplos de 3 ou 5 abaixo de 1000.*/
-$somaTres = 0;
+
+function multiplosDe(int $multiplicador, int $contador): int
+{
+    $soma = 0;
+    for ($i = 0; $i < $contador; $i++) {
+        if (($i * $multiplicador) < $contador) {
+            $multiplos = $i * $multiplicador;
+            $soma += $multiplos;
+        }
+    }
+    return $soma;
+}
+
+$multiplos3 = multiplosDe(3, 10);
+$multiplos5 = multiplosDe(5, 10);
+
+echo $multiplos3 + $multiplos5;
+/*$somaTres = 0;
 $somaCinco = 0;
 $quantParaSoma = 10;
 
@@ -18,4 +35,4 @@ for ($i = 1; $i < $quantParaSoma; $i++) {
         $somaCinco += $multCinco;
     }
 }
-echo $somaTres + $somaCinco;
+echo $somaTres + $somaCinco;*/
