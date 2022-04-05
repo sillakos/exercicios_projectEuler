4f40ff92ca22096a0087211cf3c83e5ca66c011c
Sillas Ouriques dos Santos <sillasods@gmail.com>
2022-04-05 15:00:29 -0300

    Comentários removidos Exercício 1

--- problem1.php
+++ problem1.php
@@ -5,9 +5,10 @@ A soma desses múltiplos é 23.
 
 Encontre a soma de todos os múltiplos de 3 ou 5 abaixo de 1000.*/
 
-function multiplosDe(int $multiplicador, int $contador): int
+function multiplosDe(int $multiplicador): int
 {
     $soma = 0;
+    $contador = 10;
     for ($i = 0; $i < $contador; $i++) {
         if (($i * $multiplicador) < $contador) {
             $multiplos = $i * $multiplicador;
@@ -17,22 +18,7 @@ function multiplosDe(int $multiplicador, int $contador): int
     return $soma;
 }
 
-$multiplos3 = multiplosDe(3, 10);
-$multiplos5 = multiplosDe(5, 10);
+$multiplos3 = multiplosDe(3);
+$multiplos5 = multiplosDe(5);
 
 echo $multiplos3 + $multiplos5;
-/*$somaTres = 0;
-$somaCinco = 0;
-$quantParaSoma = 10;
-
-for ($i = 1; $i < $quantParaSoma; $i++) {
-    if ($i * 3 < $quantParaSoma) {
-        $multTres = $i * 3;
-        $somaTres += $multTres;
-    }
-    if ($i * 5 < $quantParaSoma) {
-        $multCinco = $i * 5;
-        $somaCinco += $multCinco;
-    }
-}
-echo $somaTres + $somaCinco;*/
