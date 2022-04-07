a0a4d1ba41322d1f4b4f50553f1abd6901387234
Sillas Ouriques dos Santos <sillasods@gmail.com>
2022-04-07 11:29:57 -0300

    Primeira versão do exercício 3 finalizada

---
+++ problem3.php
@@ -0,0 +1,24 @@
+<?php
+
+$numero = 600851475143;
+$fatorPrimo = 0;
+$atual = 0;
+
+for ($i = 1; $i <= $numero; $i++) {
+    $divisao = $numero / $i;
+
+    if (fmod($atual, $i)) {
+        continue;
+    } else {
+        $fatorPrimo = $i;
+
+    }
+    $atual = $divisao;
+    $numero = $atual;
+}
+
+echo "O maior fator primo do número escolhido é {$fatorPrimo}" . PHP_EOL;
+
+
+
+
