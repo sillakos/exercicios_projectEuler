4f977b121ae725712c687eee03db96a7b29b442b
Sillas Ouriques dos Santos <sillasods@gmail.com>
2022-04-08 16:24:09 -0300

    Primeira refatoração do problem4.php

--- problem4.php
+++ problem4.php
@@ -1,17 +1,29 @@
 <?php
 
-$palindromo = 0;
+/*
+Um número palindrômico lê da mesma forma nos dois sentidos.
+O maior palíndromo feito do produto de dois números de 2 dígitos é 9009 = 91 × 99.
 
-for ($i = 100; $i <= 999; $i++) {
-    for ($j = 100; $j <= 999; $j++) {
-        $mult = $i * $j;
-        $numStr = strval($mult);
-        $numRev = strrev($numStr);
-        if ($numRev == $numStr) {
-            $palindromo = $mult;
-            //echo "$i x $j = {$numRev} e {$numStr}" . PHP_EOL;
+Encontre o maior palíndromo feito do produto de dois números de 3 algarismos.
+*/
+
+function MaiorPalindromoDeTresNumeros(): int
+{
+    $palindromo = 0;
+
+    for ($i = 100; $i <= 999; $i++) {
+        for ($j = 100; $j <= 999; $j++) {
+            $mult = $i * $j;
+            $numStr = strval($mult);
+            $numRev = strrev($numStr);
+            if ($numRev == $numStr) {
+                $palindromo = $mult;
+                //echo "$i x $j = {$numRev} e {$numStr}" . PHP_EOL;
+            }
         }
     }
+    return $palindromo;
 }
 
-echo $palindromo;
+$maiorPalindromo = MaiorPalindromoDeTresNumeros();
+echo $maiorPalindromo;
