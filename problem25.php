2986c3c6895fcf9d2400adc9b57fb6bf0f2c4351
Sillas Ouriques dos Santos <sillasods@gmail.com>
2022-04-13 13:07:13 -0300

    adicionando exercicio problem25.php

--- problem25.php
+++ problem25.php
@@ -1,6 +1,18 @@
 <?php
 
-class problem25
-{
+$atual = 0;
+$ultimo = 1;
+$penultimo = 0;
+$cont = 0;
 
+while (strlen($atual) < 3) {
+    $atual = $ultimo + $penultimo;
+
+    $penultimo = $ultimo;
+    $ultimo = $atual;
+
+    echo $atual . PHP_EOL;
+
+    $cont++;
 }
+echo 'F ' . ($cont + 1).PHP_EOL;
